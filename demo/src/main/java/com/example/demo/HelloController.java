0cfd6880c9bcde5af300985d64975349a8a5dc9b
dentaku <dentaku1412@gmail.com>
2021-10-16 00:52:37 +0900

    See all of https://spring.pleiades.io/guides/gs/spring-boot/#scratch

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from SpringBoot!\n";
+        return "Greetings from Spring Boot!\n";
     }
 }
