f1d586b8e527898b309846baa74f25861b07bbbf
dentaku <dentaku1412@gmail.com>
2021-10-28 23:57:59 +0900

    Commit all

--- tutorial/src/main/java/springboot/api/tutorial/TutorialApplication.java
+++ tutorial/src/main/java/springboot/api/tutorial/TutorialApplication.java
@@ -2,8 +2,10 @@ package springboot.api.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TutorialApplication {
 
 	public static void main(String[] args) {
