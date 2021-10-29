fbb91a77a8745ab2842837149cdb8f7f5cbd555e
dentaku <dentaku1412@gmail.com>
2021-10-30 01:56:23 +0900

    See all of tutorial Implement REST API with HATEOAS

---
+++ Payroll/src/main/java/com/example/Payroll/Order.java
@@ -0,0 +1,61 @@
+package com.example.Payroll;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER_ORDER")
+class Order {
+
+    private @Id @GeneratedValue Long id;
+
+    private String description;
+    private Status status;
+
+    Order() {}
+
+    Order(String description, Status status) {
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.description, this.status);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + this.id + ", description=" + this.description + ", status="
+                + this.status + '}';
+    }
+
+}
